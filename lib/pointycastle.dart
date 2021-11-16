919add5b3c783050c5440c32b70b465ac65e41b2
Marzuk Kamal <marzuk9999@gmail.com>
2021-11-17 00:44:22 +0100

    update

--- lib/pointycastle.dart
+++ lib/pointycastle.dart
@@ -13,3 +13,4 @@ library pointycastle;
 export 'package:pointycastle/api.dart';
 export 'package:pointycastle/impl.dart';
 export 'package:pointycastle/asn1.dart';
+export 'package:pointycastle/rsa_key_helper/rsa_key_helper.dart';
